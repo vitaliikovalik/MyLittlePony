c041c92329f5b08586d9867befb90f72f8d354fd
vkovaly <vitaliikovaliuk@icloud.com>
2022-09-08 13:14:06 +0300

    -wip

---
+++ MyLittlePony.AT.Framework/Configuration/Model/DriverInfo.cs
@@ -0,0 +1,14 @@
+﻿using MyLittlePony.AT.Framework.WebDriver.Enum;
+
+namespace MyLittlePony.AT.Framework.Configuration.Model
+{
+    public class DriverInfo
+    {
+        public DriverType DriverType { get; set; } = DriverType.Chrome;
+        public bool RunRemote { get; set; } = false;
+        public bool Maximize { get; set; } = true;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public RemoteDriverInfo RemoteDriverInfo { get; set; }
+    }
+}
---
+++ MyLittlePony.AT.Framework/WebDriver/DriverSettings.cs
@@ -0,0 +1,10 @@
+﻿using MyLittlePony.AT.Framework.Configuration;
+using MyLittlePony.AT.Framework.Configuration.Model;
+
+namespace MyLittlePony.AT.Framework.WebDriver
+{
+    public static class DriverSettings
+    {
+        public static DriverInfo DriverInfo => ConfigurationHelper.GetBindConfiguration<DriverInfo>(configName: "selsettings.json", section: "DriverConfiguration") ?? new DriverInfo();
+    }
+}
